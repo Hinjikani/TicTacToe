e649aac519d35acb5fc8575bf6abdc12c66b21ef
meowchii <worldendstoday@proton.me>
2025-08-25 03:00:32 +0700

    adding complete necessary functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function gameboard(){
+function gameBoard(){
   //Create gameboard
   const rows = 3
   const columns = 3
@@ -7,12 +7,12 @@ function gameboard(){
   for (let i = 0; i< rows ; i++) {
     board[i] = [];
     for (let j = 0; j<columns; j++) {
-      board[i].push("0");
+      board[i].push("");
     }
   }
   
   //Return board
-  const getboard = () => board
+  const getBoard = () => board
   
   //Put player's marker
   const mark = (row, column, player) => {
@@ -21,13 +21,74 @@ function gameboard(){
 
     if (row < 1 || row > 3 || column < 1 || row > 3) {
       console.log(`column ${column} row ${row} is out of range`)
-    } else if (board[rowIndex][columnIndex] === "0") {
+    } else if (board[rowIndex][columnIndex] === "") {
       board[rowIndex][columnIndex] = player
     } else {
       console.log(`column ${column} row ${row} is already occupied`)
     }
   }
-  return {getboard, mark}
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell));
+    console.log(boardWithCellValues);
+  };
+  return {getBoard, mark, printBoard}
 };
 
-board = gameboard()
+//Cell (represent one square within the arrays)
+
+function cell(){
+  let value = 0;
+
+  const addToken = (player) => {
+    value = player
+  }
+  const getValue = () => value;
+
+  return {addToken, getValue};
+}
+
+//gameController for controlling the flow state of the game
+
+function gameController(playerOneName = "Player one", playerTwoName = "Player two"){
+  const board = gameBoard();
+  const players =[{
+    name: playerOneName,
+    sign: "X"
+  },{
+    name: playerTwoName,
+    sign: "O"
+  }]
+  let activePlayer = players[0]
+
+  const switchPlayerTurn = () =>{
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  }
+
+  const getActivePlayer = () => activePlayer;
+  
+  //printing Current Round
+  let round = 0;
+  const printRound = () =>{
+    round++;
+    board.printBoard()
+    console.log(`Round ${round} ${getActivePlayer().name}'s turn.`)
+  };
+  
+  const playRound = (row, column) => {
+    console.log(`${getActivePlayer().name} placing ${getActivePlayer().sign} on ${row} row and ${column}`)
+    board.mark(row, column, getActivePlayer().sign)
+
+    switchPlayerTurn();
+    printRound();
+  }
+
+  printRound();
+  
+  return {
+    playRound,
+    getActivePlayer
+  }
+}
+
+const game = gameController();
