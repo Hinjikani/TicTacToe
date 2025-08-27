2269842b00dbe3a260f4a9c06f393b33721b3c7c
meowchii <worldendstoday@proton.me>
2025-08-28 03:56:46 +0700

    add win logic

--- script.js
+++ script.js
@@ -73,10 +73,64 @@ function gameController(playerOneName = "Player one", playerTwoName = "Player tw
     console.log(`Round ${round} ${getActivePlayer().name}'s turn.`)
   };
   
+  const checkWin = () => {
+    currentBoard = board.getBoard()
+    // horizontal case
+    if (currentBoard[0][0] !== "" &&
+    currentBoard[0][0] === currentBoard[0][1] &&
+    currentBoard[0][1] === currentBoard[0][2]) {
+      console.log(`${getActivePlayer().name} have won the game!!!`)
+      return("won")
+    } else if (currentBoard[1][0] !== "" &&
+    currentBoard[1][0] === currentBoard[1][1]&&
+    currentBoard[1][1] === currentBoard[1][2]){
+      console.log(`${getActivePlayer().name} have won the game!!!`)
+      return("won")
+    } else if (currentBoard[2][0] !== "" &&
+    currentBoard[2][0] === currentBoard[2][1] &&
+    currentBoard[2][1] === currentBoard[2][2]){
+      console.log(`${getActivePlayer().name} have won the game!!!`)
+      return("won") 
+    }
+    // vertical case
+    if (currentBoard[0][0] !== "" &&
+    currentBoard[0][0] === currentBoard[1][0] &&
+    currentBoard[1][0] === currentBoard[2][0]){
+      console.log(`${getActivePlayer().name} have won the game!!!`)
+      return("won")
+    } else if (currentBoard[0][1] !== "" &&
+    currentBoard[0][1] === currentBoard[1][1] &&
+    currentBoard[1][1] === currentBoard[2][1]){
+      console.log(`${getActivePlayer().name} have won the game!!!`)
+      return("won")
+    } else if (currentBoard[0][2] !== "" &&
+    currentBoard[0][2] === currentBoard[1][2] &&
+    currentBoard[1][2] === currentBoard[2][2]){
+      console.log(`${getActivePlayer().name} have won the game!!!`)
+      return("won")
+    }
+    
+    //diagonal case
+    if (currentBoard[0][0] !== "" &&
+    currentBoard[0][0] === currentBoard[1][1] &&
+    currentBoard[1][1] === currentBoard[2][2]){
+      console.log(`${getActivePlayer().name} have won the game!!!`)
+      return("won")
+    } else if (currentBoard[0][2] !== "" &&
+    currentBoard[0][2] === currentBoard[1][1] &&
+    currentBoard[1][1] === currentBoard[0][2]){
+      console.log(`${getActivePlayer().name} have won the game!!!`)
+    }
+  }
+
   const playRound = (row, column) => {
     console.log(`${getActivePlayer().name} placing ${getActivePlayer().sign} on ${row} row and ${column}`)
     let markBoard = board.mark(row, column, getActivePlayer().sign)
-    if (markBoard === false) {
+    let win = checkWin()
+
+    if (win === "won"){
+    return
+    } else if (markBoard === false) {
       printRound();
     } else {
       switchPlayerTurn();
